dac9247d9db9f313f77c94490d2139a499b7ee4d
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-10-09 14:28:45 +0300

    removed fixed postion to try on mobiles

--- src/components/WeatherBackground.tsx
+++ src/components/WeatherBackground.tsx
@@ -30,7 +30,7 @@ const WeatherBackground = async ({
   const weatherImage = getWeatherImage(condition, isDay);
 
   return (
-    <section className="fixed inset-0 -z-[1] transition-all duration-300 before:absolute before:inset-0 before:z-[1] before:bg-black/25">
+    <section className="absolute inset-0 -z-[1] transition-all duration-300 before:absolute before:inset-0 before:z-[1] before:bg-black/25">
       <h2 className="sr-only">Weather Background</h2>
       <Image
         src={weatherImage}
