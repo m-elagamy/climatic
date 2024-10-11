6c25ae4ae92cdcdbc51f540d788e586a98ac2cae
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-10-11 03:51:03 +0300

    implemented default location option successfully

--- src/components/LocationDetector.tsx
+++ src/components/LocationDetector.tsx
@@ -1,14 +1,68 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import useGeolocation from "@/hooks/useGeolocation";
+import useDefaultLocation from "@/hooks/useDefaultLocation";
+import useGeolocationPermissions from "@/hooks/useGeolocationPermissions";
 
 const LocationDetector = () => {
-  const { getGeolocation } = useGeolocation();
+  const router = useRouter();
+  const { getGeolocation, userLocationCoords, isLoading, error } =
+    useGeolocation();
+  const { userDefaultLocation, isDefaultLocationEnabled } =
+    useDefaultLocation();
+  const [hasCheckedLocation, setHasCheckedLocation] = useState(false);
+  const { isGeolocationDenied } = useGeolocationPermissions();
 
   useEffect(() => {
-    getGeolocation();
-  }, []);
+    const checkAndSetLocation = async () => {
+      if (userDefaultLocation && isDefaultLocationEnabled) {
+        router.replace(
+          `/?city=${encodeURIComponent(userDefaultLocation.city)}&lat=${userDefaultLocation.lat}&lon=${userDefaultLocation.lon}`,
+        );
+      } else if (
+        !hasCheckedLocation ||
+        (!isDefaultLocationEnabled && !isGeolocationDenied)
+      ) {
+        getGeolocation();
+      }
+      setHasCheckedLocation(true);
+    };
+
+    checkAndSetLocation();
+
+    return () => {
+      setHasCheckedLocation(false);
+    };
+  }, [
+    isDefaultLocationEnabled,
+    userDefaultLocation,
+    router,
+    getGeolocation,
+    hasCheckedLocation,
+    isGeolocationDenied,
+  ]);
+
+  useEffect(() => {
+    if (hasCheckedLocation && !isLoading) {
+      if (userLocationCoords && !isDefaultLocationEnabled) {
+        router.replace(
+          `/?lat=${userLocationCoords.lat}&lon=${userLocationCoords.lon}`,
+        );
+      } else if ((isGeolocationDenied || error) && !isDefaultLocationEnabled) {
+        router.replace("/?city=Cairo");
+      }
+    }
+  }, [
+    isLoading,
+    userLocationCoords,
+    isDefaultLocationEnabled,
+    router,
+    error,
+    isGeolocationDenied,
+    hasCheckedLocation,
+  ]);
 
   return null;
 };
--- src/hooks/useGeolocation.tsx
+++ src/hooks/useGeolocation.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useToast } from "./useToast";
 import useGeolocationPermissions from "./useGeolocationPermissions";
 import { useLocalStorage } from "./useLocalStorage";
 import delay from "@/utils/delay";
+import useDefaultLocation from "./useDefaultLocation";
 
 const useGeolocation = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   const { toast } = useToast();
   const { isGeolocationDenied, setIsGeolocationDenied } =
@@ -16,34 +19,39 @@ const useGeolocation = () => {
     lat: number;
     lon: number;
   } | null>("user-location-coords", null);
+  const { isDefaultLocationEnabled } = useDefaultLocation();
+
+  console.log(isDefaultLocationEnabled);
 
   const handleSuccess = useCallback(
     async (position: GeolocationPosition) => {
       const { latitude, longitude } = position.coords;
       await delay(2000);
-      router.replace(`/?lat=${latitude}&lon=${longitude}`);
-
       setLocationCoords({ lat: latitude, lon: longitude });
+      setIsLoading(false);
+      setError(null);
     },
-    [router, setLocationCoords],
+    [setLocationCoords],
   );
 
   const handleError = useCallback(
     async (error: GeolocationPositionError) => {
       if (error.code === error.PERMISSION_DENIED) {
         await delay(2000);
-        setTimeout(() => {
-          toast({
-            title: "Location Access Denied",
-            description:
-              "Permission to access your location was declined. Check out Cairo's weather for the moment.",
-          });
-        }, 1500);
-        router.replace("/?city=Cairo");
         setIsGeolocationDenied(true);
+
+        if (!isDefaultLocationEnabled) {
+          toast({
+            title: "Geolocation is denied",
+            description:
+              "Geolocation is denied by your browser, so we're showing you the weather for Cairo.",
+          });
+        }
+        setIsLoading(false);
+        setError(error.message);
       }
     },
-    [router, toast, setIsGeolocationDenied],
+    [toast, setIsGeolocationDenied, isDefaultLocationEnabled],
   );
 
   const handleSupportError = useCallback(async () => {
@@ -54,13 +62,15 @@ const useGeolocation = () => {
         "Geolocation is not supported by this browser, so we're showing you the weather for Cairo.",
     });
     router.replace("/?city=Cairo");
+    setIsLoading(false);
+    setError("Geolocation is not supported");
   }, [router, toast]);
 
   const getGeolocation = useCallback(() => {
+    setIsLoading(true);
+    setError(null);
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(handleSuccess, handleError, {
-        enableHighAccuracy: true,
-      });
+      navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
     } else {
       handleSupportError();
     }
@@ -70,6 +80,9 @@ const useGeolocation = () => {
     getGeolocation,
     locationCoords: userLocationCoords,
     isGeolocationDenied,
+    isLoading,
+    userLocationCoords,
+    error,
   };
 };
 export default useGeolocation;
