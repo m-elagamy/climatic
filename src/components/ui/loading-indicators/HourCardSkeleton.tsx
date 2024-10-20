2568254d14032cfab793218cf6d314cc9009fbb4
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-10-20 21:34:09 +0300

    update hour card skeleton

--- src/components/ui/loading-indicators/HourCardSkeleton.tsx
+++ src/components/ui/loading-indicators/HourCardSkeleton.tsx
@@ -2,9 +2,10 @@ import { Skeleton } from "./skeleton";
 
 const HourCardSkeleton = () => (
   <>
-    <Skeleton className="h-8 w-8 rounded-full" />
+    <Skeleton className="h-2 w-12 rounded-md" />
+    <Skeleton className="size-7 rounded-full" />
     <Skeleton className="h-4 w-8 rounded-md" />
-    <Skeleton className="h-4 w-12 rounded-md" />
+    <Skeleton className="h-2 w-12 rounded-md" />
   </>
 );
 
--- src/components/weather-widgets/hourly-forecast/CarouselHours.tsx
+++ src/components/weather-widgets/hourly-forecast/CarouselHours.tsx
@@ -52,7 +52,7 @@ const CarouselHours = ({ hoursToDisplay }: { hoursToDisplay: HourData[] }) => {
         </CarouselContent>
       </Carousel>
       <div
-        className="absolute -left-full bottom-0 h-[2px] w-full bg-gradient-to-r from-slate-200/20 to-slate-400/40 transition-all duration-300 ease-out"
+        className="absolute -left-full bottom-0 h-[2px] w-full bg-gradient-to-r from-slate-200/30 to-slate-400/50 transition-all duration-300 ease-out"
         style={{
           transform: `translate3d(${scrollProgress * 100}%,0,0)`,
         }}
