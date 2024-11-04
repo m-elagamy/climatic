ed28ebdb528d972a5c8b200466795d22b28b0a6d
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-11-04 15:35:14 +0200

    update the logic of search history to prevent duplications for the same city

--- src/utils/updateSearchHistory.ts
+++ src/utils/updateSearchHistory.ts
@@ -9,8 +9,22 @@ const updateSearchHistory = (
   ) => void,
 ) => {
   setSearchHistory((prev = []) => {
-    const newHistory = [...prev, cityData];
-    return newHistory.length > 5 ? newHistory.slice(-5) : newHistory;
+    // Check if the city already exists in the search history.
+    const isCityExit = prev.some(
+      (city) =>
+        city.name === cityData.name && city.country === cityData.country,
+    );
+
+    // If it doesn't exist, add it to the history
+    if (!isCityExit) {
+      const newHistory = [...prev, cityData];
+
+      // Limit history to the last 5 entries
+      return newHistory.length > 5 ? newHistory.slice(-5) : newHistory;
+    }
+
+    // If it exists, just return the previous state
+    return prev;
   });
 };
 
