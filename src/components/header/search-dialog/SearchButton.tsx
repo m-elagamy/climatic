0def80eb3dad2550b485697329812780d9bffd22
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2025-02-23 18:53:52 +0200

    feat: add search icon to SearchButton component

--- src/components/header/search-dialog/SearchButton.tsx
+++ src/components/header/search-dialog/SearchButton.tsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react";
 import useToggleStateContext from "@/hooks/useToggleStateContext";
 import { Button } from "@/components/ui/button";
 import { CommandShortcut } from "@/components/ui/command";
@@ -8,11 +9,12 @@ export default function SearchButton() {
   return (
     <Button
       variant="outline"
-      className="bg-gradient w-72 justify-between rounded-lg border-none p-3 tracking-wide text-muted-foreground shadow-md duration-300 hover:bg-accent/10"
+      className="bg-gradient w-72 justify-between gap-2 rounded-lg border-none p-3 tracking-wide text-muted-foreground shadow-md duration-300 hover:bg-accent/10"
       onClick={() => setIsOpen(true)}
-      aria-label="Search for a city"
+      aria-label="Find a city"
     >
-      Search for a city...
+      <Search size={14} />
+      Find a city
       <CommandShortcut className="hidden lg:inline">⌘F</CommandShortcut>
     </Button>
   );
